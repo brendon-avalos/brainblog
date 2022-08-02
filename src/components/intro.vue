9e93c7a94e7f9f2ae3b1e7cd17100481c0fdfcba
Brendon Avalos <avalos.brendon@gmail.com>
2022-08-02 13:35:58 -0400

    updated design + blur

--- src/components/intro.vue
+++ src/components/intro.vue
@@ -19,7 +19,64 @@
             <img src="@/assets/quote2.svg" alt="quote mark" />
           </div>
         </div>
-        <img src="@/assets/counter.svg" alt="counter" />
+        <svg
+          width="693"
+          height="277"
+          viewBox="0 0 693 277"
+          fill="none"
+          class="counter-svg"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none"
+        >
+          <path
+            id="o"
+            class="blur-08"
+            d="M257.917 19.7608L251.331 9.53269L247.922 11.728L241.336 1.49986L234.517 5.8904L230.127 -0.928345L223.308 3.4622L221.113 0.0528319L214.294 4.44338L212.099 1.03401L201.871 7.61983L199.676 4.21046L168.991 23.9679L171.186 27.3773L160.958 33.9631L163.154 37.3725L156.335 41.763L158.53 45.1724L151.711 49.5629L156.102 56.3817L149.283 60.7722L155.869 71.0003L152.46 73.1956L159.045 83.4237L155.636 85.619L168.808 106.075L172.217 103.88L178.803 114.108L182.212 111.913L188.798 122.141L195.617 117.75L200.007 124.569L206.826 120.179L209.021 123.588L215.84 119.197L218.035 122.607L228.264 116.021L230.459 119.43L261.143 99.6729L258.948 96.2635L269.176 89.6777L266.981 86.2683L273.799 81.8777L271.604 78.4684L278.423 74.0778L274.032 67.2591L280.851 62.8685L274.265 52.6404L277.675 50.4451L271.089 40.217L274.498 38.0218L261.327 17.5655L257.917 19.7608ZM244.795 66.7932L241.386 68.9885L247.972 79.2166L244.562 81.4119L246.757 84.8213L239.939 89.2118L242.134 92.6212L231.906 99.207L229.711 95.7976L222.892 100.188L220.697 96.7788L217.287 98.9741L210.701 88.746L207.292 90.9412L185.339 56.8475L188.749 54.6523L182.163 44.4241L185.572 42.2289L183.377 38.8195L190.196 34.4289L188 31.0196L198.228 24.4337L200.424 27.8431L207.243 23.4526L209.438 26.8619L212.847 24.6667L219.433 34.8948L222.842 32.6995L244.795 66.7932Z"
+            fill="white"
+          />
+          <path
+            id="u"
+            class="blur-15"
+            d="M361.65 2.40503V87.5601H357.595V95.67H349.485V99.725H337.32V95.67H329.21V87.5601H325.155V2.40503H288.66V99.725H292.715V107.835H296.77V115.945H304.88V120H312.99V124.055H345.43V120H353.54V115.945H357.595V111.89H361.65V103.78H365.705V120H398.145V2.40503H361.65Z"
+            fill="white"
+          />
+          <path
+            id="c"
+            class="blur-01"
+            d="M141.939 72.946H137.879V58.5135H133.818V44.0811H129.758V36.8649H125.697V29.6486H121.636V22.4324H113.515V15.2162H101.333V8H64.7879V15.2162H52.6061V22.4324H44.4848V29.6486H36.3636V36.8649H32.303V44.0811H28.2424V51.2973H24.1818V65.7297H20.1212V80.1622H16.0606V101.811H12V181.189H16.0606V202.838H20.1212V217.27H24.1818V231.703H28.2424V238.919H32.303V246.135H36.3636V253.351H44.4848V260.568H52.6061V267.784H64.7879V275H105.394V267.784H117.576V260.568H125.697V253.351H129.758V246.135H133.818V238.919H137.879V224.486H141.939V210.054H146V173.973H109.454V210.054H105.394V231.703H101.333V238.919H93.2121V246.135H72.9091V238.919H64.7879V231.703H60.7273V217.27H56.6667V195.622H52.6061V87.3784H56.6667V65.7297H60.7273V51.2973H64.7879V44.0811H72.9091V36.8649H89.1515V44.0811H97.2727V51.2973H101.333V72.946H105.394V109.027H141.939V72.946Z"
+            fill="white"
+          />
+          <path
+            id="n"
+            class="blur-12"
+            d="M254.379 139V244.686H250.31V232.943H246.241V221.2H242.172V209.457H238.103V197.714H234.034V185.971H229.966V174.229H225.897V162.486H221.828V150.743H217.759V139H173V276H209.621V174.229H213.69V185.971H217.759V197.714H221.828V209.457H225.897V221.2H229.966V232.943H234.034V244.686H238.103V256.429H242.172V268.171H246.241V276H291V139H254.379Z"
+            fill="white"
+          />
+          <path
+            id="t"
+            class="blur-05"
+            d="M303 139V167.286H367.133V249H386.867V167.286H451V139H303Z"
+            fill="white"
+          />
+          <path
+            id="r"
+            class="blur-04"
+            d="M693.32 151V98.285H689.265V90.175H685.21V86.12H677.1V82.065H656.825V73.955H668.99V69.9H681.155V65.845H685.21V61.79H689.265V53.68H693.32V33.405H689.265V25.295H685.21V21.24H681.155V17.185H673.045V13.13H660.88V9.07495H596V151H632.495V90.175H652.77V94.23H656.825V151H693.32ZM632.495 25.295H640.605V29.35H648.715V37.46H652.77V61.79H648.715V69.9H640.605V73.955H632.495V25.295Z"
+            fill="white"
+          />
+          <path
+            id="e"
+            class="blur-07"
+            d="M478.192 240.274L484.289 250.834L488.835 248.209L492.9 255.249L497.446 252.624L501.511 259.664L510.604 254.414L514.669 261.454L528.309 253.579L530.341 257.099L543.981 249.224L546.013 252.744L577.839 234.369L575.807 230.849L589.447 222.974L587.415 219.454L601.055 211.579L596.99 204.539L606.083 199.289L602.019 192.249L606.566 189.624L602.501 182.584L607.048 179.959L598.919 165.88L580.732 176.38L584.797 183.419L575.703 188.669L577.736 192.189L514.083 228.939L512.05 225.419L498.411 233.294L496.378 229.774L491.832 232.399L487.767 225.36L574.153 175.485L572.121 171.965L576.668 169.34L574.635 165.82L579.182 163.195L577.15 159.675L581.696 157.05L579.664 153.53L584.211 150.905L574.049 133.305L569.503 135.93L563.406 125.37L558.859 127.995L554.795 120.955L545.702 126.205L541.637 119.165L527.997 127.04L525.965 123.52L512.325 131.395L510.293 127.875L483.013 143.625L485.045 147.145L471.405 155.02L473.438 158.54L459.798 166.415L463.862 173.455L454.769 178.705L458.833 185.745L454.287 188.37L458.351 195.41L453.805 198.035L459.901 208.595L455.355 211.22L473.645 242.899L478.192 240.274ZM473.541 200.72L478.088 198.095L476.056 194.575L489.696 186.7L487.663 183.18L537.676 154.305L539.709 157.825L553.349 149.95L555.381 153.47L559.927 150.845L566.024 161.405L479.638 211.28L473.541 200.72Z"
+            fill="white"
+          />
+
+          <defs>
+            <clipPath id="clip0_227_167">
+              <rect width="693" height="277" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
       </div>
       <div class="info">
         <div class="about t-text">
@@ -43,7 +100,76 @@
         </div>
       </div>
       <div class="situations">
-        <img src="@/assets/situations.svg" alt="situations" />
+        <svg
+          class="situations-svg"
+          width="682"
+          height="329"
+          viewBox="0 0 682 329"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none"
+        >
+          <path
+            id="i"
+            class="blur-09"
+            d="M105 25.1428H149.576V6H105V25.1428ZM109.052 320.429H145.524V53.8571H109.052V320.429Z"
+            fill="white"
+          />
+          <path
+            id="t"
+            class="blur-17"
+            d="M165 6V42.6073H214.4V169H229.6V42.6073H279V6H165Z"
+            fill="white"
+          />
+          <path
+            id="s"
+            class="blur-03"
+            d="M3.8 147.806V168.065H11.4V178.194H22.8V188.323H41.8V198.452H64.6V208.581H76V228.839H68.4V238.968H22.8V228.839H15.2V198.452H0V249.097H3.8V279.484H11.4V299.742H22.8V309.871H34.2V320H57V309.871H68.4V299.742H79.8V279.484H87.4V259.226H91.2V178.194H87.4V157.935H79.8V147.806H68.4V137.677H49.4V127.548H26.6V117.419H15.2V97.1613H22.8V87.0323H72.2V97.1613H79.8V127.548H95V76.9032H91.2V46.5161H83.6V26.2581H72.2V16.129H60.8V6H34.2V16.129H22.8V26.2581H11.4V46.5161H3.8V66.7742H0V147.806H3.8Z"
+            fill="white"
+          />
+          <path
+            id="u"
+            class="blur-16"
+            d="M361.333 4V89.4H357.593V97.5334H350.111V101.6H338.889V97.5334H331.407V89.4H327.667V4H294V101.6H297.741V109.733H301.481V117.867H308.963V121.933H316.444V126H346.37V121.933H353.852V117.867H357.593V113.8H361.333V105.667H365.074V121.933H395V4H361.333Z"
+            fill="white"
+          />
+          <path
+            id="a"
+            class="blur-02"
+            d="M413.72 105.677V113.806H417.44V117.871H421.16V121.935H428.6V126H454.64V121.935H462.08V117.871H465.8V109.742H469.52V101.613H473.24V121.935H503V36.5807H499.28V24.387H495.56V16.258H491.84V12.1935H488.12V8.12904H480.68V4.06451H469.52V0H439.76V4.06451H428.6V8.12904H421.16V12.1935H417.44V16.258H413.72V24.387H410V44.7097H439.76V28.4516H443.48V20.3225H447.2V16.258H462.08V20.3225H465.8V28.4516H469.52V69.0968H465.8V60.9678H462.08V56.9033H454.64V52.8387H428.6V56.9033H421.16V60.9678H417.44V65.0323H413.72V73.1613H410V105.677H413.72ZM424.88 81.2903H428.6V77.2258H462.08V81.2903H469.52V89.4194H462.08V93.4839H428.6V89.4194H424.88V81.2903Z"
+            fill="white"
+          />
+          <path
+            id="i"
+            class="blur-11"
+            d="M183.668 183.834L226.753 195.378L228.852 187.545L185.767 176L183.668 183.834ZM154 310.222L189.251 319.668L219.687 206.079L184.436 196.634L154 310.222Z"
+            fill="white"
+          />
+          <path
+            id="n"
+            class="blur-06"
+            d="M536.379 138V285.343H532.31V268.972H528.241V252.6H524.172V236.228H520.103V219.857H516.034V203.485H511.966V187.115H507.897V170.743H503.828V154.372H499.759V138H455V329H491.621V187.115H495.69V203.485H499.759V219.857H503.828V236.228H507.897V252.6H511.966V268.972H516.034V285.343H520.103V301.715H524.172V318.085H528.241V329H573V138H536.379Z"
+            fill="white"
+          />
+          <path
+            id="s"
+            class="blur-13"
+            d="M590.8 221.516V234.162H598.4V240.484H609.8V246.806H628.8V253.129H651.6V259.452H663V272.097H655.4V278.419H609.8V272.097H602.2V253.129H587V284.741H590.8V303.71H598.4V316.355H609.8V322.678H621.2V329H644V322.678H655.4V316.355H666.8V303.71H674.4V291.065H678.2V240.484H674.4V227.838H666.8V221.516H655.4V215.194H636.4V208.871H613.6V202.548H602.2V189.903H609.8V183.581H659.2V189.903H666.8V208.871H682V177.259H678.2V158.29H670.6V145.645H659.2V139.322H647.8V133H621.2V139.322H609.8V145.645H598.4V158.29H590.8V170.935H587V221.516H590.8Z"
+            fill="white"
+          />
+          <path
+            id="t"
+            class="blur-10"
+            d="M517 0V32.4H588.5V126H610.5V32.4H682V0H517Z"
+            fill="white"
+          />
+          <path
+            id="o"
+            class="blur14"
+            d="M247.419 207.821V220.143H253.839V232.464H266.677V240.679H279.516V244.786H292.355V248.893H311.613V253H369.387V248.893H388.645V244.786H401.484V240.679H414.323V232.464H427.161V220.143H433.581V207.821H440V183.179H433.581V170.857H427.161V158.536H414.323V150.321H401.484V146.214H388.645V142.107H369.387V138H311.613V142.107H292.355V146.214H279.516V150.321H266.677V158.536H253.839V170.857H247.419V183.179H241V207.821H247.419ZM305.194 174.964H311.613V162.643H318.032V158.536H330.871V154.429H350.129V158.536H362.968V162.643H369.387V174.964H375.806V216.036H369.387V228.357H362.968V232.464H350.129V236.571H330.871V232.464H318.032V228.357H311.613V216.036H305.194V174.964Z"
+            fill="white"
+          />
+        </svg>
       </div>
       <div class="byline">
         <div class="t-body title">
@@ -73,6 +199,7 @@
 
 .container {
   display: flex;
+  flex-wrap: wrap;
   flex-direction: column;
   height: 100%;
   justify-content: space-between;
@@ -89,12 +216,14 @@
 .counter {
   flex-grow: 1;
   width: 100%;
+  overflow: visible;
 }
 
-.counter img {
+.counter-svg {
   height: 0;
   width: 100%;
   min-height: 100%;
+  overflow: visible;
 }
 
 .reality {
@@ -103,10 +232,10 @@
   align-items: center;
   justify-content: space-between;
   position: absolute;
-  padding: 30px;
+  padding: 5px 30px 20px 30px;
   box-sizing: border-box;
   width: 28%;
-  height: 18%;
+  height: 18vh;
   right: 8vw;
 }
 
@@ -184,12 +313,14 @@
 .situations {
   flex-grow: 1;
   width: 100%;
+  overflow: visible;
 }
 
-.situations img {
+.situations-svg {
   height: 0;
   width: 100%;
   min-height: 100%;
+  overflow: visible;
 }
 
 .byline {
@@ -211,6 +342,161 @@
 .byline .text {
   align-self: flex-end;
 }
+
+/*
+blurring animations */
+
+@keyframes blur {
+  0% {
+    filter: blur(0);
+    transform: translate3d(0, 0, 0);
+  }
+
+  7% {
+    filter: blur(0.6rem);
+    transform: translate3d(0, 0, 0);
+  }
+
+  14% {
+    filter: blur(0rem);
+    transform: translate3d(0, 0, 0);
+  }
+
+  100% {
+    filter: blur(0rem);
+    transform: translate3d(0, 0, 0);
+  }
+}
+
+/* .blurhover {
+  filter: blur(0rem) !important;
+  transition: all 0.8s ease-in-out !important;
+}
+
+.blurhover:hover {
+  filter: blur(0.5rem) !important;
+} */
+
+.blur-01 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 0;
+  overflow: visible;
+  transform: translate3d(0, 0, 0);
+}
+
+.blur-02 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 1s;
+  transform: translate3d(0, 0, 0);
+}
+
+.blur-03 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 3s;
+}
+
+.blur-04 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 5s;
+  transform: translate3d(0, 0, 0);
+}
+
+.blur-05 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 7s;
+}
+
+.blur-06 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 9s;
+  transform: translate3d(0, 0, 0);
+}
+
+.blur-07 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 10s;
+  transform: translate3d(0, 0, 0);
+}
+
+.blur-08 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 12s;
+  transform: translate3d(0, 0, 0);
+}
+
+.blur-09 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 15s;
+  transform: translate3d(0, 0, 0);
+}
+
+.blur-10 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 16s;
+  transform: translate3d(0, 0, 0);
+}
+
+.blur-11 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 18s;
+  transform: translate3d(0, 0, 0);
+}
+
+.blur-12 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 19s;
+  transform: translate3d(0, 0, 0);
+}
+
+.blur-13 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 20s;
+  transform: translate3d(0, 0, 0);
+}
+
+.blur-14 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 22s;
+  transform: translate3d(0, 0, 0);
+}
+
+.blur-15 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 25s;
+  transform: translate3d(0, 0, 0);
+}
+
+.blur-16 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 26s;
+  transform: translate3d(0, 0, 0);
+}
+
+.blur-17 {
+  animation: blur 28s infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: 27s;
+  transform: translate3d(0, 0, 0);
+}
+
+/*
+Responsive */
 
 @media only screen and (max-width: 800px) {
   .intro {
